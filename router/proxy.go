a20fc2654c0d0bc5305711516fec22379d947cb7
Xiang Li <xiang.li@coreos.com>
2014-03-27 22:52:49 -0400

    add stop function

--- router/proxy.go
+++ router/proxy.go
@@ -1,15 +1,25 @@
 package router
 
 import (
+	"fmt"
 	"net"
+	"sync"
 )
 
 type proxy struct {
+	status     string
 	connectors []*connector
 	localAddr  *net.TCPAddr
 	remoteAddr *net.TCPAddr
 	listener   net.Listener
+	sync.Mutex
 }
+
+const (
+	initialized = "initialized"
+	running     = "running"
+	stopped     = "stopped"
+)
 
 func newProxy(laddrStr, raddrStr string) (*proxy, error) {
 	laddr, err := net.ResolveTCPAddr("tcp", laddrStr)
@@ -26,6 +36,7 @@ func newProxy(laddrStr, raddrStr string) (*proxy, error) {
 		connectors: make([]*connector, 0),
 		localAddr:  laddr,
 		remoteAddr: raddr,
+		status:     initialized,
 	}
 
 	return p, nil
@@ -33,7 +44,16 @@ func newProxy(laddrStr, raddrStr string) (*proxy, error) {
 
 func (p *proxy) start() error {
 	var err error
+	p.Lock()
+	if p.status != initialized {
+		defer p.Unlock()
+		return fmt.Errorf("the status of proxy is not initialized [%s]", p.status)
+	}
+
+	p.status = running
 	p.listener, err = net.Listen("tcp", p.localAddr.String())
+	p.Unlock()
+
 	if err != nil {
 		return err
 	}
@@ -51,12 +71,41 @@ func (p *proxy) start() error {
 			}
 
 			c := newConnector(one, other)
+			if err := p.addConnector(c); err != nil {
+				return
+			}
+
 			c.connect()
 		}(one)
-
 	}
 }
 
-func (p *proxy) stop() {
+// todo(xiangli) Graceful shutdown
+func (p *proxy) stop() error {
+	p.Lock()
+	defer p.Unlock()
 
+	if p.status != running {
+		return fmt.Errorf("the status of proxy is not running [%s]", p.status)
+	}
+
+	p.status = stopped
+	p.listener.Close()
+	for _, c := range p.connectors {
+		c.disconnect()
+	}
+	p.connectors = nil
+	return nil
+}
+
+func (p *proxy) addConnector(c *connector) error {
+	p.Lock()
+	defer p.Unlock()
+
+	if p.status != running {
+		return fmt.Errorf("the status of proxy is not running [%s]", p.status)
+	}
+
+	p.connectors = append(p.connectors, c)
+	return nil
 }
